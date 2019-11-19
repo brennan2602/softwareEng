d4558eec50e9dcb4e3f544d5aedd459b8a11a30e
RYAN BRENNAN <R.BRENNAN15@nuigalway.ie>
2019-11-19 11:42:00 +0000

    update student class

--- src/main/java/nuigalway/app/my_app/Student.java
+++ src/main/java/nuigalway/app/my_app/Student.java
@@ -1,6 +1,13 @@
 package nuigalway.app.my_app;
 
 public class Student {
-private String Name;
+private String name;
 private int age;
+private String DOB;
+
+public Student(String name, int age, String DOB) {
+this.name=name;
+this.age=age;
+this.DOB=DOB;
+}
 }
