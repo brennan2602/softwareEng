bf9714f452c8b552e249d3bc6aad3d10c96b4fcb
RYAN BRENNAN <R.BRENNAN15@nuigalway.ie>
2019-11-19 10:54:16 +0000

    initial commit

---
+++ src/main/java/nuigalway/app/my_app/App.java
@@ -0,0 +1,13 @@
+package nuigalway.app.my_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
